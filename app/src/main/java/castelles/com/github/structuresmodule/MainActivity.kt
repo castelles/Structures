0f9c81f60b90ae7082dd11c69459eaabd308a1cf
Caio Telles <cast@icomp.ufam.edu.br>
2021-05-18 22:47:03 -0400

    chainedList

---
+++ app/src/main/java/castelles/com/github/structuresmodule/MainActivity.kt
@@ -0,0 +1,11 @@
+package castelles.com.github.structuresmodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
